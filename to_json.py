775ec8e01350a3059e259352ba8d0a8a614fc4b5
Cannon Lock <clock@wisc.edu>
2022-06-03 14:39:37 -0500

    Add combined json

--- to_json.py
+++ to_json.py
@@ -106,7 +106,7 @@ def export_combined():
 
     d_institutions = df_institutions.to_dict(orient="index")
 
-    d = {'igwn' : {"computeSites": d_compute_sites, "institutions": d_institutions}}
+    d = {'IGWN' : {"computeSites": d_compute_sites, "institutions": d_institutions}}
 
     with open("data/collaborations.json", "w") as fp:
         json.dump(d, fp)
